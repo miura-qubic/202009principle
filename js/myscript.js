6f8ef162a085b9397a9fcef97ba98ec2811907d1
miura <miura@qu-bic.jp>
2020-09-18 19:08:34 +0900

    トップページ：完成（pc）

--- js/myscript.js
+++ js/myscript.js
@@ -18,10 +18,33 @@ const tabletwidth = 1025;
 
 $(function () {
 	$("body").removeClass("preload");
-// JQueryの範囲
 
+	// JQueryの範囲
 
+	$(window).on('scroll', function() {
+		if ($(this).scrollTop() > 500) {
+				$('#pagetop').fadeIn('fast');
+		} else {
+				$('#pagetop').fadeOut('fast');
+		}
+	});
 
+	// スムーススクロール 
+	var urlHash = location.hash;
+	if (urlHash) {
+		$('body,html').stop().scrollTop(0);
+		setTimeout(function () {
+			var target = $(urlHash);
+			var position = target.offset().top;
+			$('body,html').stop().animate({ scrollTop: position }, 500);
+		}, 100);
+	}
+	$('a[href^="#"]').click(function () {
+		var href = $(this).attr("href");
+		var target = $(href);
+		var position = target.offset().top;
+		$('body,html').stop().animate({ scrollTop: position }, 500);
+	});
 
 
 });
