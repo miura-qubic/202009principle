32e64ab7f22a2d970c33247345e82f5d05b8f961
Yoshida <qubic.co.ltd@gmail.com>
2020-09-29 17:03:39 +0900

    スムーススクロール 修正

--- js/myscript.js
+++ js/myscript.js
@@ -58,20 +58,24 @@ $(function () {
 
 	// スムーススクロール 
 	var urlHash = location.hash;
-	if (urlHash) {
-		$('body,html').stop().scrollTop(0);
-		setTimeout(function () {
-			var target = $(urlHash);
+	// if (urlHash) {
+		// $('body,html').stop().scrollTop(0);
+		// setTimeout(function () {
+		// 	var target = $(urlHash);
+		// 	var position = target.offset().top;
+		// 	$('body,html').stop().animate({ scrollTop: position }, 500);
+		// 	return false;
+		// }, 100);
+	// } else {
+		
+		$('a[href^="#"]').click(function () {
+			var href = $(this).attr("href");
+			var target = $(href);
 			var position = target.offset().top;
 			$('body,html').stop().animate({ scrollTop: position }, 500);
-		}, 100);
-	}
-	$('a[href^="#"]').click(function () {
-		var href = $(this).attr("href");
-		var target = $(href);
-		var position = target.offset().top;
-		$('body,html').stop().animate({ scrollTop: position }, 500);
-	});
+			return false;
+		});
+	// }
 
 	// 個人スポンサーのスライダー
 	var swiperMain = new Swiper('.top06 .swiper-container', {
